053d3c8be5aba880ebae0e19a2f4b4e4d5606b54
Johnsonlee <johnsonlee28@gmail.com>
2017-03-13 16:36:47 +0800

    implement carts index function

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,9 @@ class Product < ApplicationRecord
   mount_uploader :image, ImageUploader
 
   def add_product_to_cart(product)
-    
+    ci = cart_items.build
+    ci.product = product
+    ci.quantity = 1
+    ci.save
+  end
 end
