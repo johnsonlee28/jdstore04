207e920184a81cd8c64243148e6cc2bf06bfb017
Johnsonlee <johnsonlee28@gmail.com>
2017-03-15 14:56:27 +0800

    delete an item from cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,5 @@
 class Product < ApplicationRecord
   mount_uploader :image, ImageUploader
 
-  def add_product_to_cart(product)
-    ci = cart_items.build
-    ci.product = product
-    ci.quantity = 1
-    ci.save
-  end
+
 end
